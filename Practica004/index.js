5a6c8fe35dcd6c6174044f389b5cf3c07b6fd394
sos1819jpcc <sevillanocarmona@gmail.com>
2019-02-27 13:40:04 +0000

    parte final

--- Practica004/index.js
+++ Practica004/index.js
@@ -1,3 +1,19 @@
 var cool = require("cool-ascii-faces");
 
-console.log(cool());
+var express = require("express ");
+
+var app = express();
+var port = (process.env.PORT || 8080);
+
+app.get("/", (request, response) => 
+    {
+     response.send(cool());
+     console.log("New request received");
+    }
+);
+
+app.listen(port, () => {
+    console.log("Server TRULY ready!");
+});
+
+console.log("Server ready");
