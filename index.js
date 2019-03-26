3dedf458c5532392a11503dabc848ef9a28541bd
sos1819jpcc <sevillanocarmona@gmail.com>
2019-03-26 18:07:31 +0000

    appi save

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
 
-var mdbURL = "mongodb://test:";
+
 
 var app = express();
 
@@ -11,6 +11,254 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+var beer_stats;
+/*
+const uri_beer_stats = "mongodb+srv://test:test@sos1819-04-afg-ysoip.mongodb.net/test?retryWrites=true";
+const client_beer_stats = new MongoClient(uri_beer_stats, { useNewUrlParser: true });
+var mdbURL = "mongodb://test:";
+
+client_happiness_stats.connect(err => {
+    
+    if(err) console.log("Error:", err);
+    happiness_stats = client_happiness_stats.db("sos1819").collection("happiness-stats");
+    console.log("Connected!");
+  
+});
+*/
+
+/* JP
+  ======================
+ |  API REST DE JUAN PEDRO  |
+  ======================
+*/
+
+//DOCUMENTACION /api/v1/suicide-rates/docs (REDIRIGE A LA DOCUMENTACIÓN DE LA API REST)
+
+
+
+//CREACIÓN DEL OBJETO "BeerStat"
+var BeerStat = {
+    initBeerStat: function(country, year, rating, variation, countryConsumition) {
+        this.country = country;
+        this.year = year;
+        this.rating = rating;
+        this.variation = variation;
+        this.countryConsumition = countryConsumition;
+    }
+}
+
+
+//GET /api/v1/beer-consumed-BeerStats/loadInitialData
+app.get("/api/v1/beer-consumed-BeerStats/loadInitialData", (req, res) => {
+    
+        var beerStat1 = Object.create(BeerStat);
+        var beerStat2 = Object.create(BeerStat);
+        var beerStat3 = Object.create(BeerStat);
+        var beerStat4 = Object.create(BeerStat);
+        var beerStat5 = Object.create(BeerStat);
+        
+        beerStat1.initBeerStat("españa", 2016, 84.8, 2, 3909);
+        beerStat2.initBeerStat("alemania", 2016, 104.2, -0.5, 8412);
+        beerStat3.initBeerStat("lituania", 2016, 88.7, -8.4, 257);
+        beerStat4.initBeerStat("corea del sur", 2016, 42.8, 0.3, 2160);
+        beerStat5.initBeerStat("reino unido", 2016, 67.7, 1.6, 4373);
+        
+        beer_stats.find({}).toArray( (err, beer_stats_array) => {
+                
+                if (err) console.log("FATAL ERROR !!: ", err);
+                
+                if (beer_stats_array.length == 0) {
+                    
+                    beer_stats.insert(beerStat1);
+                    beer_stats.insert(beerStat2);
+                    beer_stats.insert(beerStat3);
+                    beer_stats.insert(beerStat4);
+                    beer_stats.insert(beerStat5);
+                    console.log("Request accepted, creating new resources in database.");
+                    res.sendStatus(201);
+                    
+                } else {
+                    
+                    console.log("FATAL ERROR !!: Data Base is not empty.");
+                    res.sendStatus(409);
+                    
+                }
+                
+            }
+        );
+    }
+);
+
+
+//GET /api/v1/beer-consumed-BeerStats (DEVUELVE UNA LISTA CON TODOS LOS RECURSOS)
+app.get("/api/v1/beer-consumed-BeerStats", (req, res) => {
+    
+          beer_stats.find({}).toArray( (err, beer_stats_array) => {
+                
+                if (err) {
+                    console.log("FATAL ERROR !! : ", err);
+                } else {
+                    console.log("Request accepted, sending resources from database.");
+                }
+                
+                res.send(beer_stats_array);
+                
+            }
+        );
+    
+    }
+);
+
+//POST /api/v1/beer-consumed-BeerStats (CREA UN NUEVO RECURSO)
+app.post("/api/v1/beer-consumed-BeerStats", (req, res) => {
+        
+        var newStat = req.body;
+        
+        beer_stats.find(newStat).toArray( (err, beer_stats_array) => {
+            
+                if(err) console.log("FATAL ERROR !!: ", err);
+                
+                if(beer_stats_array == 0){
+                    
+                    beer_stats.insert(newStat);
+                    console.log("Request accepted, creating new resource in database.");
+                    res.sendStatus(201);
+                    
+                } else {
+                    
+                    console.log("FATAL ERROR !!: Resource already exists in the database.");
+                    res.sendStatus(409);
+                    
+                }
+            
+            }
+        );
+        
+    }
+);
+
+//GET /api/v1/beer-consumed-BeerStats/--reurso-- (DEVUELVE UN RECURSO CONCRETO)
+app.get("/api/v1/beer-consumed-BeerStats/:country", (req, res) => {
+        
+        var country = req.params.country;
+        
+         beer_stats.find( {"country": country} ).toArray( (err, beer_stats_array) => {
+            
+                if(err) console.log("FATAL ERROR !!: ", err);
+                
+                if(beer_stats_array.length  > 0){
+                    
+                    console.log("Request accepted, sending resource from database.");
+                    res.send(beer_stats_array);
+                    
+                } else {
+                    
+                    console.log("Request accepted, removing resource of database.");
+                    res.sendStatus(404);
+                    
+                }
+            
+            }
+        );
+        
+    }
+);
+
+//DELETE /api/v1/beer-consumed-BeerStats/--reurso-- (BORRA UN RECURSO CONCRETO)
+app.delete("/api/v1/beer-consumed-BeerStats/:country", (req, res) => {
+        
+        var country = req.params.country;
+        var found = false;
+        
+        beer_stats.find( {"country": country} ).toArray( (err, beer_stats_array) =>{
+            
+                if(err) console.log("FATAL ERROR: ", err);
+                
+                if(beer_stats_array.length > 0){
+                    
+                    beer_stats.remove(beer_stats_array[0]);
+                    console.log("Request accepted, removing resource of database.");
+                    res.sendStatus(200);
+                    
+                } else {
+                    
+                    console.log("FATAL ERROR !!: Resource not found in database.");
+                    res.sendStatus(404);
+                    
+                }
+            
+            }
+        );
+        
+    }
+);
+
+//PUT /api/v1/beer-consumed-BeerStats/--reurso-- (ACTUALIZA UN RECURSO CONCRETO)
+app.put("/api/v1/beer-consumed-BeerStats/:country", (req, res) => {
+        
+        var country = req.params.country;
+        var updatedBeerStat = req.body;
+        
+        beer_stats.find( {"country": country} ).toArray( (err, beer_stats_array) => {
+                
+                if(err) console.log("FATAL ERROR: ", err);
+                
+                if(beer_stats_array.length > 0){
+                    
+                    beer_stats.update( {"country": country}, updatedBeerStat );
+                    console.log("Request accepted, updating resource of database.");
+                    res.sendStatus(200);
+                    
+                } else {
+                    
+                    console.log("FATAL ERROR : Resource not found in database.");
+                    res.sendStatus(404);
+                    
+                }
+            
+            }
+        );
+        
+    }
+);
+
+//POST /api/v1/beer-consumed-BeerStats/--reurso-- (ERROR METODO NO PERMITIDO)
+app.post("/api/v1/beer-consumed-BeerStats/:country", (req, res) => {
+        
+        console.log("<h1>FATAL ERROR !!: Method not Allowed.</h1>");
+        res.sendBeerStatus(405);
+    }
+);
+
+//PUT /api/v1/beer-consumed-BeerStats (ERROR METODO NO PERMITIDO)
+app.put("/api/v1/beer-consumed-BeerStats", (req, res) => {
+        
+        console.log("<h1>FATAL ERROR !!: Method not Allowed.</h1>");
+        res.sendBeerStatus(405);
+        
+    }
+);
+
+//DELETE /api/v1/beer-consumed-BeerStats (BORRA TODOS LOS RECURSOS)
+app.delete("/api/v1/beer-consumed-BeerStats", (req, res) => {
+        
+        beer_stats.remove({});
+        console.log("<h1>Request accepted, removing all resources of database.</h1>");
+        res.sendBeerStatus(200);
+    }
+);
+
+//SERVIDOR A LA ESCUCHA DE PETICIONES
+app.listen(port, () => { console.log("Server Ready and Listen in port", port); });
+
+
+
+
+
+
+
+
+/*
 var contacts = [{
     name: "peter",
     phone: "123456",
@@ -36,7 +284,7 @@ app.post("/contacts", (req,res)=>{
     
     contacts.push(newContact)
     
-    res.sendStatus(201);
+    res.sendBeerStatus(201);
 });
 
 
@@ -46,7 +294,7 @@ app.delete("/contacts", (req,res)=>{
     
     contacts =  [];
 
-    res.sendStatus(200);
+    res.sendBeerStatus(200);
 });
 
 
@@ -63,7 +311,7 @@ app.get("/contacts/:name", (req,res)=>{
     if (filteredContacts.length >= 1){
         res.send(filteredContacts[0]);
     }else{
-        res.sendStatus(404);
+        res.sendBeerStatus(404);
     }
 
 });
@@ -89,10 +337,10 @@ app.put("/contacts/:name", (req,res)=>{
     });
     
     if (found == false){
-        res.sendStatus(404);
+        res.sendBeerStatus(404);
     }else{
         contacts = updatedContacts;
-        res.sendStatus(200);
+        res.sendBeerStatus(200);
     }
 
 });
@@ -114,10 +362,10 @@ app.delete("/contacts/:name", (req,res)=>{
     });
     
     if (found == false){
-        res.sendStatus(404);
+        res.sendBeerStatus(404);
     }else{
         contacts = updatedContacts;
-        res.sendStatus(200);
+        res.sendBeerStatus(200);
     }
 
 });
@@ -127,3 +375,4 @@ app.delete("/contacts/:name", (req,res)=>{
 app.listen(port, () => {
     console.log("Super server ready on port " + port);
 });
+*/
