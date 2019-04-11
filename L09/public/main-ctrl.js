84c645519d0609217cf87cf6f03ecfe993583fd3
sos1819jpcc <sevillanocarmona@gmail.com>
2019-04-11 10:28:57 +0000

    Commit for order

--- L09/public/main-ctrl.js
+++ L09/public/main-ctrl.js
@@ -8,14 +8,57 @@ angular
                 console.log("[Contacts App] Requesting Data to Contacts APP.");
                 
                 var URL = "/api/v1/contacts";
+                refresh();
+                
+                function(refresh){
+                    $http.get(URL).then(function(res){
+                        console.log("[Contacts App] Data Received: " + JSON.stringify(res.data, null, 2));
+                        $scope.contacts = res.data;
+                    });
+                }
+                
+              
                 
                 $http.get(URL).then(function(res){
-                    
                         console.log("[Contacts App] Data Received: " + JSON.stringify(res.data, null, 2));
                         $scope.contacts = res.data;
                         
                     }
                 );
+                
+                $scope.addContacts = function(){
+                    var newContact = $scope.newContact;
+                    console.log("Adding a new contac: " + JSON.stringify(newContact));
+                    $http
+                        .post(URL, newContact)
+                        .then(function (response){
+                    
+                    console.log("POST Response"
+                                + response.status + " "
+                                + response.data);
+                    });
+                }
+                
+                
+                //DELETE
+                $scope.deleteContact = function(){
+                    var newContact = $scope.newContact;
+                    console.log("Adding a new contac: " + JSON.stringify(newContact));
+                    $http
+                        .post(URL, newContact)
+                        .then(function (response){
+                    
+                    console.log("POST Response"
+                                + response.status + " "
+                                + response.data);
+                    });
+                }
+                
+                
+                
+                
+                
+                
                 
             }
         ]
